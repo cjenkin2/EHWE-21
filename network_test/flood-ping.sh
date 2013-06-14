2ceadf4496b87f33383f3df6d52fa9c42252a26b
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-06-14 14:47:26 -0500

    Argument parsing for flood-ping.sh

--- network_test/flood-ping.sh
+++ network_test/flood-ping.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
-if [ $# -ne 1 ]
+if [[ ($# -ne 1) && ($# -ne 2) ]]; then
 	echo "usage: $0 <hostname> [time]"
 	exit 65
 fi
 
-count=$(sudo ping -i 0 -w $DEADLINE $myHost | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+HOSTNAME=$1
+if [[ $# -eq 1 ]]; then
+	DEADLINE=60
+else
+	DEADLINE=$2
+fi
+
+count=$(sudo ping -i 0 -w $DEADLINE $HOSTNAME | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
         echo "Count is $count"
         if [ $count -eq 0 ]; then
                 echo "Could not reach host $myHost"
