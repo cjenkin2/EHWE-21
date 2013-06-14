a23a9b832a2aa7dab7823fa129dca497157a6fd9
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-06-14 15:09:00 -0500

    Added primitive error catching in flood-ping.sh

--- network_test/flood-ping.sh
+++ network_test/flood-ping.sh
@@ -12,8 +12,15 @@ else
 	DEADLINE=$2
 fi
 
-count=$(sudo ping -i 0 -w $DEADLINE $HOSTNAME | grep 'received' | awk -F',' '{ print $2 }' | awk '{ print $1 }')
-        echo "Count is $count"
-        if [ $count -eq 0 ]; then
-                echo "Could not reach host $myHost"
-        fi
+STATUS_MSG=$(sudo ping -i 0 -w $DEADLINE $HOSTNAME | grep 'received')
+COUNT=$(echo $STATUS_MSG | awk -F',' '{ print $2 }' | awk '{ print $1 }')
+echo "ping: $STATUS_MSG" > flood-ping.log
+
+if [ "$STATUS_MSG" = "" ]; then
+	COUNT=0
+fi
+
+if [ $COUNT -eq 0 ]; then
+        echo "Could not reach host $HOSTNAME!"
+	exit 1
+fi
