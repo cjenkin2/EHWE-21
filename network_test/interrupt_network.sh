ebdc86307a9bb5c1d75ab66b0506fd15d812c959
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-06-23 14:33:37 -0500

    Tested network and gpu together, troubleshooted problems running these from masterscript

---
+++ network_test/interrupt_network.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+sudo killall -9 run_network_test.sh
+sudo killall -9 test_connection.sh
+sudo killall -9 dhclient
+sudo killall -9 wpa_supplicant
+sudo killall -9 wget
+sudo killall -9 ping
