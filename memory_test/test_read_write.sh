32d4078c988004d538af0fa7eefb84bc27838b1b
chris-genesi <chris.jenkins@genesi-usa.com>
2013-06-26 11:37:25 -0500

    Configured test_read_write.sh to work with md5sum_compare.sh

--- memory_test/test_read_write.sh
+++ memory_test/test_read_write.sh
@@ -3,15 +3,14 @@
 if [[ $# -ne 1 ]]
 then
 	echo "usage: $0 outfile"
+	echo ""
 	echo "$0 writes 2200 copies of lorem ipsum to the specified file location, "
 	echo "then checks to see that the resulting file matches the pre-generated md5sum"
+	exit 65 # bad params
 fi
 
 OUTFILE=$1
 
 zcat loremipsum_2200.txt.gz > $OUTFILE
-zcat loremipsum_2200.txt.gz > loremipsum_2200.txt
-md5sum -c $OUTFILE
 
-#cleanup
-rm loremipsum_2200.txt
+echo $(./md5sum_compare.sh loremipsum_2200.txt.md5sum $OUTFILE)
