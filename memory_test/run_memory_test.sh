36bd42fd5652a7328bd45963c95ae21cb9d7f584
chris-genesi <chris.jenkins@genesi-usa.com>
2013-06-26 13:05:17 -0500

    Started integrating master memory_test script with test_read_write.sh, mounting devices to local directory

--- memory_test/run_memory_test.sh
+++ memory_test/run_memory_test.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
 
-DEVICES="/dev/sda /dev/mmcblk0"
+#DEVICES="/dev/sda /dev/mmcblk0"
+DEVICES="/dev/mmcblk0"
 BAD_BLK_LOG="./badblocks.log"
+MNT="./mount_point"
+
 
 # test for bad blocks in devices
 for DEVICE in $DEVICES
 do
 	sudo badblocks -o $BAD_BLK_LOG $DEVICE 
+
+	sudo mount $DEVICE $MNT
+	ret=$?
+
+	if [[ $ret -eq 0 ]] # successfully mounted
+	then
+		./test_read_write.sh
+	fi
+
+	sudo umount $DEVICE
 done
