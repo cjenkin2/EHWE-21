ed236cd5689cfc4d5db7806a18d7a7b0e605e008
cjenkin2 <chris.jenkins@genesi-usa.com>
2013-07-03 16:04:57 -0500

    Updated and tested persistent memory tests

--- filesystem_test/run_memory_test.sh
+++ filesystem_test/run_memory_test.sh
@@ -1,34 +1,43 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "usage: $0 <device>"
+	exit 65 # bad arguments
+fi
+
 #DEVICES="/dev/sda /dev/mmcblk0"
-DEVICES="/dev/mmcblk0"
 BAD_BLK_LOG="./badblocks.log"
 BENCHMARK_LOG="./benchmark.log"
 MNT="./mount_point"
+DEVICE=$1
 
-for DEVICE in $DEVICES
-do
+	sudo umount "$DEVICE"
+
 	# test blocks
-	sudo badblocks -o $BAD_BLK_LOG $DEVICE 
+	echo $(date) >> $BAD_BLK_LOG
+#	sudo badblocks -o $BAD_BLK_LOG $DEVICE
+	echo "------------------------------" >> $BAD_BLK_LOG 
 
 	# test read/write
-	sudo mount "$DEVICE"p1 $MNT # always mount partition 1
+	sudo mount $DEVICE $MNT 
 	ret=$?
+
+	echo "Tried to mount $DEVICE on $MNT: $ret"
 
 	if [[ $ret -eq 0 ]] # successfully mounted
 	then
 		# sudo ./test_read_write.sh $MNT/loremipsum.txt
-		sudo zcat loremipsum.txt.gz > $MNT/loremipsum.txt
-		sudo umount "$DEVICE"p1
-		sudo mount "$DEVICE"p1 $MNT # assumes that if it worked once it will twice
-		echo $(./md5sum_compare.sh loremipsum.txt.md5sum $MNT/loremipsum.txt)
+		sudo chmod 666 $MNT/*
+		sudo bash -c "zcat loremipsum.txt.gz > $MNT/loremipsum.txt"
+		sudo umount "$DEVICE"
+		sudo mount "$DEVICE" $MNT # assumes that if it worked once it will twice
+		echo $(sudo ./md5sum_compare.sh loremipsum.txt.md5sum $MNT/loremipsum.txt)	# read
+		sudo hdparm -t $DEVICE >> $BENCHMARK_LOG
+		# write
+		sudo dd count=1k bs=1M if=/dev/zero of=$MNT/nothing.dat >> $BENCHMARK_LOG
 	fi
 
 	# test performance
-	# read
-	hdparm -t $DEVICE > $BENCHMARK_LOG
-	# write
-	sudo dd count=1k bs=1M if=/dev/null of=$MNT/nothing.dat
 
-	sudo umount $DEVICE
-done
+	sudo umount "$DEVICE"
