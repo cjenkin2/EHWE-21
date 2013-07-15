a185702deda0c5e3a5a0f20f106fd56c023402bc
cjenkin2 <root@veigar.(none)>
2013-07-15 15:32:47 -0500

    Updated netwokr test to run for fixed amount of time

--- network_test/download_ubuntu.sh
+++ network_test/download_ubuntu.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
+if [ $# -ne 1 ]
+then
+	echo "usage: $0 TIME"
+	exit 65 # bad args
+fi
+
+WGET_TIME=$1
+
 wget -a wget-ubuntu.log -b http://www.ubuntu.com/start-download?distro=desktop&bits=32&release=lts &
 
-sleep 1200
+sleep $WGET_TIME
 
 killall wget
 rm start-download*
--- network_test/run_network_test.sh
+++ network_test/run_network_test.sh
@@ -2,18 +2,25 @@
 
 if [ $# -lt 1 ]
 then
+	WGET_TIME=600
+else
+	WGET_TIME=$1
+fi
+
+if [ $# -lt 2 ]
+then
 	if [ -e "wpa.conf" ]
 	then
-		./test_connection.sh wpa.conf
+		./test_connection.sh $WGET_TIME wpa.conf
 	else
-		echo "usage: $0 [ wpa_conf_file | ssid [ passphrase ] ]"
-		echo "When run without arguments tries to read network configuration from ./wpa.conf"
+		echo "usage: $0 [time] [ wpa_conf_file | ssid [ passphrase ] ]"
+		echo "When run without arguments tries to read network configuration from ./wpa.conf, uses time of 10 minutes"
 	fi
-elif [ -e $1 ] # assumes this is the proper wpa.conf
+elif [ -e $2 ] # assumes this is a proper wpa.conf
 then
-	./test_connection.sh $1
-else # assumes this is the name of the network connection
-	wpa_passphrase $1 $2 > wpa.tmp.conf
-	./test_connection.sh wpa.tmp.conf
+	./test_connection.sh $WGET_TIME $2
+else # assumes $2 is the name of the network connection
+	wpa_passphrase $2 $3 > wpa.tmp.conf
+	./test_connection.sh $WGET_TIME wpa.tmp.conf
 	rm wpa.tmp.conf
 fi
